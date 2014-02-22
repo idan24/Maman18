427a1a2ddac31100d6bec378f0df68af48d4aabc
idan24 <idansass@gmail.com>
2014-02-22 17:32:12 +0200

    Added RedBlackTree

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,7 +2,22 @@
  * Created by Idans on 22/02/14.
  */
 public class Main {
-    public static void main(String args[]) {
-        System.out.print("sdfs");
+    public static void main( String [ ] args ) {
+        RedBlackTree t = new RedBlackTree( );
+        final int NUMS = 400000;
+        final int GAP  =  35461;
+
+        System.out.println( "Checking... (no more output means success)" );
+
+        for( int i = GAP; i != 0; i = ( i + GAP ) % NUMS )
+            t.insert( new Integer( i ) );
+
+        if( ((Integer)(t.findMin( ))).intValue( ) != 1 ||
+                ((Integer)(t.findMax( ))).intValue( ) != NUMS - 1 )
+            System.out.println( "FindMin or FindMax error!" );
+
+        for( int i = 1; i < NUMS; i++ )
+            if( ((Integer)(t.find( new Integer( i ) ))).intValue( ) != i )
+                System.out.println( "Find error1!" );
     }
 }
